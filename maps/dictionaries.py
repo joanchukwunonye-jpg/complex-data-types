dba04dc490106ee6c51582ab98e104ebf69ed9f7
Joan Chukwunonye <joan.chukwunonye@gmail.com>
2026-01-20 00:56:52 +0100

    Completed python dictionaries exercises

--- maps/dictionaries.py
+++ maps/dictionaries.py
@@ -10,7 +10,10 @@
 #
 ###################### YOUR CODE BELOW THIS LINE #######################
 
-newDict = None
+newDict = {
+  "key": "value"
+}
+
 
 ########################################################################
 
@@ -42,9 +45,18 @@ import datetime
 #
 ###################### YOUR CODE BELOW THIS LINE #######################
 
-ageInDays = None
-ageInMonths = None
-ageInYears = None
+dob = datetime.date(
+	getMyAge["DOB"]["year"],
+	getMyAge["DOB"]["month"],
+	getMyAge["DOB"]["day"]
+)
+
+today = datetime.date.today()
+
+ageInDays = (today - dob).days
+ageInMonths = ageInDays / 12
+ageInYears = ageInDays / 365
+
 
 ########################################################################
 
@@ -77,7 +89,10 @@ import copy
 #
 ###################### YOUR CODE BELOW THIS LINE #######################
 
-deepDictionaryCopy = None
+deepDictionaryCopy = copy.deepcopy(deepDictionary)
+deepDictionaryCopy["name"] = "Kravis Lott"
+deepDictionaryCopy["siblings"].pop()
+
 
 ########################################################################
 
@@ -113,6 +128,13 @@ studentCopy = dict(student)
 #
 ###################### YOUR CODE BELOW THIS LINE #######################
 
+student["grade"] += 1
+student["gpa"] -= 0.15
+student["courses"].append({
+	"subject": "MATH",
+	"level": 204
+})
+student["absences"] = 1
 
 
 ########################################################################
